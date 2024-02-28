5b308c3f8527e77aee8b7c11bdfb9448ca9069bc
Seppura-y <y7513414@gmail.com>
2024-02-29 00:20:05 +0800

    1. 创建FolderAdapter，用回收器视图显示测试列表的folder信息

---
+++ app/src/main/java/com/example/donutktplayer/FolderAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.donutktplayer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.donutktplayer.databinding.FolderViewBinding
+
+class FolderAdapter(private val context: Context, private var folderList: ArrayList<String>) : RecyclerView.Adapter<FolderAdapter.FolderHolder>() {
+    class FolderHolder(binding: FolderViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        val folderName = binding.folderNameFV
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FolderAdapter.FolderHolder {
+        return FolderHolder(FolderViewBinding.inflate(LayoutInflater.from(context), parent, false))
+    }
+
+
+    override fun onBindViewHolder(holder: FolderAdapter.FolderHolder, position: Int) {
+        holder.folderName.text = folderList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return folderList.size
+    }
+}
--- app/src/main/java/com/example/donutktplayer/FoldersFragment.kt
+++ app/src/main/java/com/example/donutktplayer/FoldersFragment.kt
@@ -5,11 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.donutktplayer.databinding.FragmentFoldersBinding
 
 class FoldersFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_folders, container, false)
+        val binding = FragmentFoldersBinding.inflate(inflater, container, false)
+
+        val tempList = ArrayList<String>()
+        tempList.add("a folder")
+        tempList.add("b folder")
+        tempList.add("c folder")
+        tempList.add("d folder")
+        tempList.add("e folder")
+
+        binding.folderRecyclerView.setHasFixedSize(true)
+        binding.folderRecyclerView.setItemViewCacheSize(10)
+        binding.folderRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.folderRecyclerView.adapter = FolderAdapter(requireContext(), tempList)
+        return binding.root
     }
 
 }
