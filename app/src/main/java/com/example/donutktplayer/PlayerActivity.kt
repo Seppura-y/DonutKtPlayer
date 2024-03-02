9b363191a05213093be54f5279563c40ea9e1e05
Seppura-y <y7513414@gmail.com>
2024-03-02 14:11:54 +0800

    1. PlayerActivity添加next previous的按键响应

--- app/src/main/java/com/example/donutktplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/donutktplayer/PlayerActivity.kt
@@ -2,6 +2,7 @@ package com.example.donutktplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.donutktplayer.databinding.ActivityPlayerBinding
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -47,8 +48,15 @@ class PlayerActivity : AppCompatActivity() {
             if(player.isPlaying) pauseVideo()
             else playVideo()
         }
+        binding.previousBtn.setOnClickListener {
+            nextPrevVideo(isNext = false)
+        }
+        binding.nextBtn.setOnClickListener {
+            nextPrevVideo(isNext = true)
+        }
     }
     private fun createPlayer(){
+        try{player.release()}catch (e: Exception){}
         binding.videoTitle.text = playerList[position].title
         // isSelected设置为true，才能开启滚动效果
         binding.videoTitle.isSelected = true
@@ -71,6 +79,38 @@ class PlayerActivity : AppCompatActivity() {
     private fun pauseVideo(){
         binding.playPauseBtn.setImageResource(R.drawable.play_icon)
         player.pause()
+    }
+
+    private fun nextPrevVideo(isNext: Boolean = true){
+        if(isNext) setPosition(isIncrement = true)
+        else setPosition(isIncrement = false)
+
+        createPlayer()
+    }
+
+    private fun setPosition(isIncrement: Boolean = true){
+        if(isIncrement)
+        {
+            if(playerList.size - 1 == position)
+            {
+                position = 0
+            }
+            else
+            {
+                ++position
+            }
+        }
+        else
+        {
+          if(position == 0)
+          {
+              position = playerList.size - 1
+          }
+          else
+          {
+            --position
+          }
+        }
     }
 
     override fun onDestroy() {
