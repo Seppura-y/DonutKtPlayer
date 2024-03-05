4bd0bff348333ce8f75e31bd6ace6b551bbfd813
Seppura-y <y7513414@gmail.com>
2024-03-05 17:16:27 +0800

    1. 添加PIP功能

--- app/src/main/java/com/example/donutktplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/donutktplayer/PlayerActivity.kt
@@ -1,9 +1,14 @@
 package com.example.donutktplayer
 
 import android.app.AlertDialog
+import android.app.AppOpsManager
+import android.app.PictureInPictureParams
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.media.audiofx.LoudnessEnhancer
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -315,7 +320,36 @@ class PlayerActivity : AppCompatActivity() {
                         if(sleepTime < 120) sleepTime += 5
                         bindingSpeed.speedText.text = "$sleepTime Min"
                     }}
-            }
+            } // bindingMF.sleepTimerBtn.setOnClickListener
+
+            bindingMF.pipModeBtn.setOnClickListener {
+                val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+                val status = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    appOps.checkOpNoThrow(AppOpsManager.OPSTR_PICTURE_IN_PICTURE, android.os.Process.myUid(), packageName) == AppOpsManager.MODE_ALLOWED
+                } else {
+                    false
+                }
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    if (status) {
+                        this.enterPictureInPictureMode(PictureInPictureParams.Builder().build())
+                        dialog.dismiss()
+                        binding.playerView.hideController()
+                        playVideo()
+                    }
+                    else{
+                        val intent = Intent("android.settings.PICTURE_IN_PICTURE_SETTINGS",
+                            Uri.parse("package:$packageName"))
+
+                        startActivity(intent)
+                    }
+                }else{
+                    Toast.makeText(this, "Feature Not Supported", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                    playVideo()
+                }
+
+            } //bindingMF.pipModeBtn.setOnClickListener
         }
     }
     private fun createPlayer(){
